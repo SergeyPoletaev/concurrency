ba4c47a2c28b16b625b13c99037f510a5276e3b7
spoletaev <info.elekit@yandex.ru>
2023-08-01 17:37:27 +0300

    Задание 3. Практика: экзекьюторы и очереди

--- src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
+++ src/main/java/course/concurrency/m5_streams/ThreadPoolTask.java
@@ -1,16 +1,24 @@
 package course.concurrency.m5_streams;
 
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 public class ThreadPoolTask {
 
     // Task #1
     public ThreadPoolExecutor getLifoExecutor() {
-        return null;
+        BlockingQueue<Runnable> deque = new LinkedBlockingDeque<>(20) {
+            @Override
+            public Runnable take() throws InterruptedException {
+                return super.takeLast();
+            }
+        };
+        return new ThreadPoolExecutor(10, 100, 60L, TimeUnit.SECONDS, deque);
     }
 
     // Task #2
     public ThreadPoolExecutor getRejectExecutor() {
-        return null;
+        return new ThreadPoolExecutor(8, 8, 0L, TimeUnit.SECONDS,
+                new SynchronousQueue<>(),
+                new ThreadPoolExecutor.DiscardPolicy());
     }
 }
